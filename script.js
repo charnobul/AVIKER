ff8cb0994b8c5858cb860d7b30171e9016939449
charnobul <168744540+charnobul@users.noreply.github.com>
2025-01-19 00:12:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCP7tSUVqBWJCaAXVguwRluRxgxDnirdmA",
+    authDomain: "jdfsjnfjcdshjfcbh.firebaseapp.com",
+    projectId: "jdfsjnfjcdshjfcbh",
+    storageBucket: "jdfsjnfjcdshjfcbh.firebasestorage.app",
+    messagingSenderId: "848070443991",
+    appId: "1:848070443991:web:6b60eab40fa314db390192",
+    measurementId: "G-68JLRVYVP4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
 document.addEventListener('DOMContentLoaded', () => {
     const stars = document.querySelectorAll('.star');
     const review = document.getElementById('review');
@@ -16,17 +36,25 @@ document.addEventListener('DOMContentLoaded', () => {
     submitButton.addEventListener('click', () => {
         const reviewText = review.value;
         if (selectedRating && reviewText) {
-            const reviewItem = document.createElement('li');
-            reviewItem.classList.add('review-item');
-            reviewItem.innerHTML = `<strong>Оценка: ${selectedRating}</strong><p>${reviewText}</p>`;
-            reviewsList.appendChild(reviewItem);
+            const newReviewRef = push(ref(database, 'reviews'));
+            newReviewRef.set({
+                rating: selectedRating,
+                text: reviewText
+            });
             review.value = '';
             stars.forEach(s => s.classList.remove('selected'));
             selectedRating = 0;
             alert('Ваш отзыв был отправлен!');
-            // Здесь можно добавить код для отправки данных на сервер
         } else {
             alert('Пожалуйста, выберите оценку и оставьте отзыв.');
         }
+    });
+
+    onChildAdded(ref(database, 'reviews'), (snapshot) => {
+        const reviewData = snapshot.val();
+        const reviewItem = document.createElement('li');
+        reviewItem.classList.add('review-item');
+        reviewItem.innerHTML = `<strong>Оценка: ${reviewData.rating}</strong><p>${reviewData.text}</p>`;
+        reviewsList.appendChild(reviewItem);
     });
 });
